dc845eb617474fb2b68104e6d4a796ed196231d5
angelporlan <angeelporlan4@gmail.com>
2026-03-09 20:43:00 +0100

    chore: agrega configuracion inicial de Tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        deepSpace: '#050505',
+        neonPurple: '#b026ff',
+        electricCyan: '#00f0ff',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
